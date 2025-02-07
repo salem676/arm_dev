b8863289b1748e3f6407bd3c200ccbf6f2c865d8
Garcia <sp2532bb0b@rpi-buster-armhf>
2025-02-07 21:58:17 +0000

    module3

---
+++ modules/module3/src/HelloWorld.s
@@ -0,0 +1,16 @@
+.text
+
+.global main
+main:
+	SUB sp, sp, #4
+	STR lr, [sp]
+
+	LDR r0, =helloWorld
+	BL printf
+
+	LDR lr, [sp]
+	ADD sp, sp, #4
+	MOV pc, lr
+.data
+	helloWorld: .asciz "Hello World\n"
+#End main
