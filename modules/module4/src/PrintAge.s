598a14b11e11e02ace1e77b1a60c656f6f2b9a00
Garcia <sp2532bb0b@rpi-buster-armhf>
2025-02-16 17:40:44 +0000

    loading final versions

--- modules/module4/src/PrintAge.s
+++ modules/module4/src/PrintAge.s
@@ -15,61 +15,25 @@ main:
 	LDR r0, =prompt1
 	BL printf
 
-	# debug printing registers before scanf
-	MOV r2, r0
-	MOV r3, r1
-	BL debug_registers_before_scanf
-
 	# read the user integer age
 	LDR r0, =format1
 	LDR r1, =num1
 	BL scanf
-	
-	# debug printing registers after scanf
-	MOV r2, r0
-	MOV r3, r1
-	BL debug_registers_after_scanf
 
 	# print the user age
 	
 	LDR r0, =output1
 	LDR r1, =num1
-	LDR r1, [r1, #0]	
+	LDR r1, [r1]	
 	BL printf
-
-	# debug printing registers after printf
-	MOV r2, r0
-	MOV r3, r1
-	BL debug_registers_after_printf
 
 	# pop the stack record	
 	LDR lr, [sp, #0]
 	ADD sp, sp, #4
-	MOV pc, lr
-
-debug_registers_before_scanf:
-	# this function prints register before scanf
-	LDR r0, =debug_msg1
-	BL printf
-	MOV pc, lr
-
-debug_registers_after_scanf:
-	# this function prints register after scanf
-	LDR r0, =debug_msg2
-	BL printf
-	MOV pc, lr
-
-debug_registers_after_printf:
-	# this function prints register after printf
-	LDR r0, =debug_msg3
-	BL printf
-	MOV pc, lr
+	BX lr
 
 .data
 	prompt1: .asciz "Enter your age: "
 	format1: .asciz "%d"
 	num1: .word 0
 	output1: .asciz "You are %d years old\n"
-	debug_msg1: .asciz "Before scanf: r0=%p, r1=%p\n"
-	debug_msg2: .asciz "After scanf: r0=%p, r1=%p\n"
-	debug_msg3: .asciz "After printf: r0=%p, r1=%p\n"
--- modules/module4/src/PrintQuotedString.s
+++ modules/module4/src/PrintQuotedString.s
@@ -6,7 +6,6 @@
 #
 .text
 .global main
-.extern exit
 main:
 	# push the stack record
 	SUB sp, sp, #4
@@ -16,32 +15,16 @@ main:
 	LDR r0, =prompt1
 	BL printf
 
-	# debug printing registers before scanf
-	LDR r2, =format1
-	LDR r3, =string1
-	BL debug_registers_before_scanf
-
 	# read the user string
 	LDR r0, =format1
 	LDR r1, =string1
 	BL scanf
-
-	# debug printing registers after scanf
-
-	LDR r2, =format1
-	LDR r3, =string1
-	BL debug_registers_after_scanf
 
 	# print the user quoted string
 	
 	LDR r0, =output1
 	LDR r1, =string1
 	BL printf
-
-	# debug printing registers after printf
-	LDR r2, =output1
-	LDR r3, =string1
-	BL debug_registers_after_printf
 
 	# pop the stack record	
 	LDR lr, [sp, #0]
@@ -51,32 +34,10 @@ main:
 	LDR r0, =0
 	BL exit
 
-debug_registers_before_scanf:
-	# this function prints register before scanf
-	LDR r0, =debug_msg1
-	BL printf
-	BX lr
-
-debug_registers_after_scanf:
-	# this function prints register after scanf
-	LDR r0, =debug_msg2
-	BL printf
-	BX lr
-
-debug_registers_after_printf:
-	# this function prints register after printf
-	LDR r0, =debug_msg3
-	BL printf
-	BX lr
-
 
 .data
 	#%99[^\n] allows us to read characters until \n
-	.align 4
 	prompt1: .asciz "Enter your string: "
-	format1: .asciz "%s"
+	format1: .asciz "%99[^\n]"
 	string1: .space 100
 	output1: .asciz "\"%s\"\n"
-	debug_msg1: .asciz "\nBefore scanf: r0=%p, r1=%p\n"
-	debug_msg2: .asciz "\nAfter scanf: r0=%p, r1=%p\n"
-	debug_msg3: .asciz "\nAfter printf: r0=%p, r1=%p\n"
