6f7e07d9bf3987c80d7b672447bab05fb94287e5
Garcia <sp2532bb0b@rpi-buster-armhf>
2025-02-16 17:13:39 +0000

    having trouble

--- modules/module4/src/PrintQuotedString.s
+++ modules/module4/src/PrintQuotedString.s
@@ -6,6 +6,7 @@
 #
 .text
 .global main
+.extern exit
 main:
 	# push the stack record
 	SUB sp, sp, #4
@@ -16,8 +17,8 @@ main:
 	BL printf
 
 	# debug printing registers before scanf
-	MOV r2, r0
-	MOV r3, r1
+	LDR r2, =format1
+	LDR r3, =string1
 	BL debug_registers_before_scanf
 
 	# read the user string
@@ -27,8 +28,8 @@ main:
 
 	# debug printing registers after scanf
 
-	MOV r2, r0
-	MOV r3, r1
+	LDR r2, =format1
+	LDR r3, =string1
 	BL debug_registers_after_scanf
 
 	# print the user quoted string
@@ -38,40 +39,44 @@ main:
 	BL printf
 
 	# debug printing registers after printf
-	MOV r2, r0
-	MOV r3, r1
+	LDR r2, =output1
+	LDR r3, =string1
 	BL debug_registers_after_printf
 
 	# pop the stack record	
 	LDR lr, [sp, #0]
 	ADD sp, sp, #4
-	MOV pc, lr
+
+	# explicit exit
+	LDR r0, =0
+	BL exit
 
 debug_registers_before_scanf:
 	# this function prints register before scanf
 	LDR r0, =debug_msg1
 	BL printf
-	MOV pc, lr
+	BX lr
 
 debug_registers_after_scanf:
 	# this function prints register after scanf
-	LDR r0, debug_msg2
+	LDR r0, =debug_msg2
 	BL printf
-	MOV pc, lr
+	BX lr
 
-debug_registers_after_printf
+debug_registers_after_printf:
 	# this function prints register after printf
-	LDR r0, debug_msg3
+	LDR r0, =debug_msg3
 	BL printf
-	MOV pc, lr
+	BX lr
 
 
 .data
 	#%99[^\n] allows us to read characters until \n
+	.align 4
 	prompt1: .asciz "Enter your string: "
-	format1: .asciz "%99[^\n]"
+	format1: .asciz "%s"
 	string1: .space 100
 	output1: .asciz "\"%s\"\n"
-	debug_msg1: .asciz "Before scanf: r0=%p, r1=%p\n"
-	debug_msg2: .asciz "After scanf: r0=%p, r1=%p\n"
-	debug_msg3: .asciz "After printf: r0=%p, r1=%p\n"
+	debug_msg1: .asciz "\nBefore scanf: r0=%p, r1=%p\n"
+	debug_msg2: .asciz "\nAfter scanf: r0=%p, r1=%p\n"
+	debug_msg3: .asciz "\nAfter printf: r0=%p, r1=%p\n"
--- modules/module4/src/PrintStringWTabs.s
+++ modules/module4/src/PrintStringWTabs.s
@@ -11,14 +11,38 @@ main:
 	SUB sp, sp, #4
 	STR lr, [sp, #0]
 
+	# debug printing registers before printf
+	MOV r2, r0
+	MOV r3, r1
+	BL debug_registers_before_printf
+
 	# print the output string
 	LDR r0, =output1
 	BL printf
+
+	# debug printing registers after printf
+	MOV r2, r0
+	MOV r3, r1
+	BL debug_registers_after_printf
 	
 	# pop the stack and return
 	LDR lr, [sp, #0]
 	ADD sp, sp, #4
 	MOV pc, lr
 
+debug_registers_before_printf:
+	# this function prints register before printf
+	LDR r0, =debug_msg1
+	BL printf
+	MOV pc, lr
+
+debug_registers_after_printf:
+	# this function prints register after printf
+	LDR r0, =debug_msg2
+	BL printf
+	MOV pc, lr
+
 .data
 	output1: .asciz "Z\t76\tL\n"
+	debug_msg1: .asciz "Before printf: r0=%p, r1=%p\n"
+	debug_msg2: .asciz "After printf: r0=%p, r1=%p\n"
