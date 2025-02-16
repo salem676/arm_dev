4d70c8207bd0a9aa600b6a39f3c5cd7e2f063fe3
Garcia <sp2532bb0b@rpi-buster-armhf>
2025-02-16 16:20:17 +0000

    adding pointer printing

--- modules/module4/src/PrintQuotedString.s
+++ modules/module4/src/PrintQuotedString.s
@@ -15,26 +15,63 @@ main:
 	LDR r0, =prompt1
 	BL printf
 
+	# debug printing registers before scanf
+	MOV r2, r0
+	MOV r3, r1
+	BL debug_registers_before_scanf
+
 	# read the user string
 	LDR r0, =format1
 	LDR r1, =string1
 	BL scanf
 
+	# debug printing registers after scanf
+
+	MOV r2, r0
+	MOV r3, r1
+	BL debug_registers_after_scanf
+
 	# print the user quoted string
 	
 	LDR r0, =output1
 	LDR r1, =string1
-	# LDR r1, [r1, #0] this line is removed because string1 is 
-	# the address of the string	
 	BL printf
+
+	# debug printing registers after printf
+	MOV r2, r0
+	MOV r3, r1
+	BL debug_registers_after_printf
 
 	# pop the stack record	
 	LDR lr, [sp, #0]
 	ADD sp, sp, #4
 	MOV pc, lr
+
+debug_registers_before_scanf:
+	# this function prints register before scanf
+	LDR r0, =debug_msg1
+	BL printf
+	MOV pc, lr
+
+debug_registers_after_scanf:
+	# this function prints register after scanf
+	LDR r0, debug_msg2
+	BL printf
+	MOV pc, lr
+
+debug_registers_after_printf
+	# this function prints register after printf
+	LDR r0, debug_msg3
+	BL printf
+	MOV pc, lr
+
+
 .data
 	#%99[^\n] allows us to read characters until \n
 	prompt1: .asciz "Enter your string: "
 	format1: .asciz "%99[^\n]"
 	string1: .space 100
 	output1: .asciz "\"%s\"\n"
+	debug_msg1: .asciz "Before scanf: r0=%p, r1=%p\n"
+	debug_msg2: .asciz "After scanf: r0=%p, r1=%p\n"
+	debug_msg3: .asciz "After printf: r0=%p, r1=%p\n"
